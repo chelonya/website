f95fb76ac2c6f50321cc9a5f291c76168bc80a50
chelonya <volunteercoordination@asvpa.org>
2026-01-26 19:43:48 -0600

    deploy to custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,8 @@ import vue from '@astrojs/vue';
 import astroI18next from 'astro-i18next';
 
 export default defineConfig({
-  site: 'https://chelonya.github.io',
+  site: 'https://www.asvpa.org/',
   // Only use base path in production - always end with /
-  base: import.meta.env.PROD ? '/asvpa/' : '/',
   trailingSlash: 'ignore',
   integrations: [vue(), astroI18next()],
   output: 'static',
